ae6edbc1c8b0afd3e815072706549b9cdb75b46e
Benjamin Assadsolimani <benjamin.assadsolimani@gmail.com>
2018-12-29 15:37:39 +0100

    Update jsdoc to ts documentation syntax

--- lib/tag-definition.ts
+++ lib/tag-definition.ts
@@ -1,11 +1,12 @@
 /**
  * @author Benjamin Assadsolimani
- * Tag Definition class
+ * Tag definition class
  */
 
 'use strict';
 
 export class TagDefinition {
+  /** Name of the tag */
   name: string;
   applies: Function;
   map: Function;
@@ -13,13 +14,13 @@ export class TagDefinition {
 
   /**
    * Create a new TagDefinition instance.
-   * @param {string} name - Name of the tag.
-   * @param {Object} options - Tag options.
-   * @param {Function} [options.applies] - Function to check whether the tag value applies -> the configuration file is loaded.
-   * @param {Function} [options.map] - Function to transform a tag value before further processing it.
-   * @param {Function} [options.score] - Function to compute a score value for the tag.
+   * @param name - Name of the tag.
+   * @param options - Tag options.
+   * @param options.applies - Function to check whether the tag value applies -> the configuration file is loaded.
+   * @param options.map - Function to transform a tag value before further processing it.
+   * @param options.score - Function to compute a score value for the tag.
    */
-  constructor(name, options) {
+  constructor(name: string, options: {applies?: Function, map?: Function, score?: Function} = {}) {
     this.name = name;
     this.applies = options.applies || function () { return false; };
     this.map = options.map || function (val) { return val; };
