33d2721db3dc0a917588f0ddfd0ea9ea69660193
Benjamin Assadsolimani <benjamin.assadsolimani@gmail.com>
2018-12-30 17:06:47 +0100

    Add snapshot tests

--- test/flex-conf.test.ts
+++ test/flex-conf.test.ts
@@ -5,10 +5,25 @@
  import { FlexConf } from '../src/flex-conf';
  import { join } from 'path';
 
- test('test 1', () => {
-   const flexConf = new FlexConf(join(__dirname, 'configs'), {
-     folderTags: true
-    });
-    const config = flexConf.final();
+ describe('Correct loading of a specific configuration file', () => {
+   test('Tag-based configuration file loading', () => {
+     for (let tag1 = 1; tag1 <= 2; tag1 += 1) {
+      for (let tag2 = 1; tag2 <= 2; tag2 += 1) {
+        const flexConf = new FlexConf(join(__dirname, 'configs'), {
+          folderTags: true,
+          tagDefinitions: {
+            tag1: {
+             applies: val => val === `val${tag1}`
+            },
+            tag2: {
+             applies: val => val === `val${tag2}`
+            }
+          }
+         });
+         const config = flexConf.final();
+         expect(config.name1).toMatchSnapshot();
+      }
+     }
 
+   });
  });
